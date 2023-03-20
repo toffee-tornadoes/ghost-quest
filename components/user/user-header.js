e2f306c47bc3284c3f16d39896464b9abbb5c954
aaron-su98 <mr.aaron.su@gmail.com>
2023-03-20 15:34:05 -0400

    Add templates for user components and pages

---
+++ components/user/user-header.js
@@ -0,0 +1,8 @@
+const UserHeader = () => {
+    return (
+        <div>This is the header for the user.</div>
+    )
+    
+}
+
+export default UserHeader;
--- pages/user/[userId]/visited.js
+++ pages/user/[userId]/visited.js
@@ -1,1 +1,8 @@
 // ghostquest.com/user/[id]/visited
+const UserPlacesVisitedPage = () => {
+    return (
+        <div>This is where the user's visited places go.</div>
+    )
+}
+
+export default UserPlacesVisitedPage;
