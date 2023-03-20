e33c89ed1ac25a0c6f8f7a853e1e3bd3da466b80
John Madrid <jmadr37@gmail.com>
2023-03-20 15:40:15 -0400

    Set up basic google map component

---
+++ components/nav.js
@@ -0,0 +1,10 @@
+const Nav = () => {
+  return (
+    <>
+      <h1 className="bg-black text-white text-center">GhostQuest</h1>
+      <div className="bg-black text-white text-center">This is the Navbar</div>
+    </>
+  );
+};
+
+export default Nav;
