239fb7b3c4db05c76d212828de960aadf0b325a4
Erik Tonnesen <estonnesen@gmail.com>
2023-03-20 15:45:55 -0400

    added auth and search pages and components

--- pages/user/index.js
+++ pages/user/index.js
@@ -1,4 +1,19 @@
 // ghostquest.com/user/
 
+const { default: LoginForm } = require("@/components/auth/login-form");
+const { default: LoginHeader } = require("@/components/auth/login-header");
+const { Fragment } = require("react");
+
 // Auth UI component - IF user is not logged in... display
 // Login or Sign-in component
+
+const UserLoginPage = () => {
+  return (
+    <Fragment>
+      <LoginHeader />
+      <LoginForm />
+    </Fragment>
+  );
+};
+
+export default UserLoginPage;
