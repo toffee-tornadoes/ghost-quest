2bd56b10759b98b28d9b87a2176f8f81fb1bc90d
Erik Tonnesen <estonnesen@gmail.com>
2023-03-21 17:25:00 -0400

    added categories

---
+++ components/categories.js
@@ -0,0 +1,23 @@
+const Categories = () => {
+  return (
+    <div id="categories" className="justify-center mt-4 w-1/2 grid">
+      Or filter by category!
+      <div className="flex justify-around">
+        <button className="bg-yellow-400 w-32 m-1 p-2 pl-4 pr-4 text-gray-700 hover:text-white rounded-md text-center
+        ">Favorites</button>
+        <button className="bg-yellow-400 w-32 m-1 p-2 pl-4 pr-4 text-gray-700 hover:text-white rounded-md text-center
+        ">Fear Factor</button>
+      </div>
+      <div className="flex justify-around">
+        <button className="bg-yellow-400 w-32 m-1 p-2 pl-4 pr-4 text-gray-700 hover:text-white rounded-md text-center
+        ">Rating</button>
+        <button className="bg-yellow-400 w-32 m-1 p-2 pl-4 pr-4 text-gray-700 hover:text-white rounded-md text-center
+        ">Distance</button>
+      </div>
+      <button className="bg-green-400 m-1 p-2 pl-4 pr-4 text-gray-700 hover:text-white rounded-md text-center
+        ">Random Destination!</button>
+    </div>
+  );
+};
+
+export default Categories;
--- components/layout.js
+++ components/layout.js
@@ -7,6 +7,7 @@ import { supabase } from "@/lib/supabaseClient";
 import { useLoadScript } from "@react-google-maps/api";
 import { useState, useEffect } from "react";
 import Header from "./header";
+import NavIcon from "./icons/nav-icon";
 import Map from "./map";
 import Nav from "./nav";
 import NavButton from "./nav-button";
@@ -18,19 +19,19 @@ const fetchData = async () => {
 
 const Layout = ({ children }) => {
   const [navUp, setNavUp] = useState(false);
-  const [height, setHeight] = useState('h-36')
-  const [hidden, setHidden] = useState('hidden')
+  const [height, setHeight] = useState("h-36");
+  const [hidden, setHidden] = useState("hidden");
   const [locations, setLocations] = useState([]);
 
   const clickHandler = () => {
     if (!navUp) {
       setNavUp(!navUp);
       setHeight("h-4/5");
-      setHidden('')
+      setHidden("");
     } else {
       setNavUp(!navUp);
-      setHeight('h-36');
-      setHidden('hidden')
+      setHeight("h-36");
+      setHidden("hidden");
     }
   };
 
@@ -51,15 +52,20 @@ const Layout = ({ children }) => {
       <Map locations={locations} />
       <div
         id="layoutDiv"
-        className={`flex-col z-1 overflow-auto transition-height duration-300 ease-in-out rounded-t-2xl ${height} pt-4 w-full gap-y-10 text-center text-white flex absolute bg-black bottom-0`}
+        className={`flex-col z-1 items-center overflow-auto transition-height duration-300 ease-in-out rounded-t-2xl ${height} pt-4 w-full gap-y-10 text-center text-white flex fixed bg-black bottom-0`}
       >
-        <button onClick={clickHandler} className="hover:text-slate-300">
-          Swipe Up
-        </button>
-        <div className={`${hidden}`} id="pageContainer">
-          {children}
+        <div onClick={clickHandler} className="flex cursor-pointer hover:scale-110 justify-center fixed">
+          <NavIcon />
         </div>
-        <NavButton />
+        <div
+          className={`${hidden} pt-10 h-full justify-between w-full p-3 flex flex-col`}
+          id="pageContainer"
+        >
+          {children}
+          <div className="pb-5">
+            <NavButton />
+          </div>
+        </div>
       </div>
     </>
   );
