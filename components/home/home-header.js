952b3dd65befcaae153f346b2efdf649eb307be9
Erik Tonnesen <estonnesen@gmail.com>
2023-03-23 17:57:18 -0400

    minor updates

--- components/home/home-header.js
+++ components/home/home-header.js
@@ -2,18 +2,16 @@
 
 const HomeHeader = () => {
   return (
-    <div className="flex justify-between" id="locListingHeader">
+    <div className="flex justify-between" id="homeHeader">
       <div className="m-2 text-left text-3xl">
         <h1>WELCOME TO GHOST QUEST</h1>
         <div className="text-slate-500 italic text-base">
           <h1>
             Your adventure starts here...
           </h1>
+          <hr />
         </div>
       </div>
-      {/* <div className="p-2">
-        <BackIcon />
-      </div> */}
     </div>
   );
 };
