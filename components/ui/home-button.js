1333c78a8c75655d862115429e2a60d491001afc
Erik Tonnesen <estonnesen@gmail.com>
2023-03-23 15:42:27 -0400

    more ui updates. home page functional

--- components/ui/home-button.js
+++ components/ui/home-button.js
@@ -1,20 +1,21 @@
 import Link from "next/link";
-// import FavoriteIcon from "../icons/favorite-icon 3";
+import ForwardIcon from "../icons/forward-icon";
 
-const HomeButton = ({text, link}) => {
+const HomeButton = ({ text, link }) => {
   return (
-    <div className="w-3/4 flex flex-row justify-between p-2 border-solid border-2 hover:bg-slate-900 rounded-md m-2 hover:border-purple-600 hover:cursor-pointer border-slate-700">
+
+    <div className={`w-3/4 flex flex-row p-2 border-solid border-2 hover:bg-slate-900 rounded-md m-2 hover:border-purple-600 hover:cursor-pointer border-slate-700`}>
       <Link
-        className="w-full text-base text-left text-slate-300 hover:text-purple-400"
+        className="w-full text-base text-slate-300 hover:text-purple-400"
         href={`/${link}`}
       >
-        <h2 className="flex">
+        <h2 className="text-center">
           {text}
         </h2>
       </Link>
-      {/* <button>
-        <FavoriteIcon />
-      </button> */}
+      <button>
+        <ForwardIcon />
+      </button>
     </div>
   );
 };
