6ad0c7b507f0320959f1c74b1d75b8f99592d4ed
John Madrid <jmadr37@gmail.com>
2023-03-24 11:39:06 -0400

    Error handling for when a user has no visited/favorited locations

--- pages/user/[id]/visited.js
+++ pages/user/[id]/visited.js
@@ -1,30 +1,34 @@
 // ghostquest.com/user/[id]/visited
-import {useUser} from '@supabase/auth-helpers-react'
-import { useState } from 'react'
-import { supabase } from '@/lib/supabaseClient'
-import LocationListingCard from '@/components/locations/loc-listing-card';
+import { useUser } from "@supabase/auth-helpers-react";
+import { useState } from "react";
+import { supabase } from "@/lib/supabaseClient";
+import LocationListingCard from "@/components/locations/loc-listing-card";
 
- export const getServerSideProps = async (context) => {
-  const {id}= context.params
-  const { data} = await supabase
+export const getServerSideProps = async (context) => {
+  const { id } = context.params;
+  const { data } = await supabase
     .from("user_locations")
-    .select('*,locations(*)')
-    .eq('profile_id', id )
-    .eq('has_visited', true)
+    .select("*,locations(*)")
+    .eq("profile_id", id)
+    .eq("has_visited", true);
 
   return {
     props: {
-      data
+      data,
     },
   };
 };
 
-const UserPlacesVisitedPage = ({data}) => {
-    if(data){
-    console.log(data)}
+const UserPlacesVisitedPage = ({ data }) => {
+  if (data.length > 0) {
     return (
-        <div><LocationListingCard locations={[data[0].locations]}/></div>
-    )
-}
+      <div>
+        <LocationListingCard locations={[data[0].locations]} />
+      </div>
+    );
+  } else {
+    return <p>No Visited Locations</p>;
+  }
+};
 
 export default UserPlacesVisitedPage;
