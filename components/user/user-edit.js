5eeecb53e5a4b12e23ae69d34e4875220637c96e
rochbman <rochbman@gmail.com>
2023-03-27 14:21:07 -0400

    file upload input

--- components/user/user-edit.js
+++ components/user/user-edit.js
@@ -4,52 +4,80 @@ import { supabase } from "@/lib/supabaseClient";
 const UserEdit = () => {
   const user = useUser();
 
-  const [thisusername, setThisUsername] = useState("");
-  const [thisFullname, setThisFullname] = useState("");
-  const [thisphone, setThisPhone] = useState("");
+  const [username, setUsername] = useState("");
+  const [full_name, setFullname] = useState("");
+  const [phone, setPhone] = useState("");
 
   const [loading, setLoading] = useState(true);
 
-  async function updateProfile({ thisusername }) {
+  async function updateProfile({ username }) {
     try {
-      console.log(thisusername)
-      const { data, error } = await supabase
-        .from("profiles")
-        .insert([{ username: `${thisusername}` }]).eq('id', `${user.id}`);
-      console.log(user);
-      if (error) throw error;
-      alert("Profile updated!");
+      const updates = {
+        id: user.id,
+        username,
+        full_name,
+        updated_at: new Date(),
+      };
+
+      let { error } = await supabase.from("profiles").upsert(updates);
+      if (error) {
+        throw error;
+      }
     } catch (error) {
-      alert("Error updating the data!");
-      console.log(error);
+      alert(error.message);
     }
   }
 
+    async function storeProfilePic({ event }) {
+      try {
+        const avatarFile = event.target.files[0];
+        const { data, error } = await supabase.storage
+          .from("avatars")
+          .upload("public/avatar1.png", avatarFile, {
+            cacheControl: "3600",
+            upsert: false,
+          });
+        if (error) {
+          throw error;
+        }
+      } catch (error) {
+        alert(error.message);
+      }
+    }
+
   return (
     <div>
-      {/* <div>
-        <label htmlFor="thisFullname">fullname</label>
-        <input
-          id="thisFullname"
-          type="text"
-          value={thisFullname || ''}
-          onChange={(e) => setThisFullname(e.target.value)}
-        />
-      </div> */}
       <div>
-        <label htmlFor="thisusername">Username</label>
+        <label htmlFor="full_name">fullname</label>
         <input
-          id="thisusername"
+          id="full_name"
           type="text"
-          value={thisusername || ""}
-          onChange={(e) => setThisUsername(e.target.value)}
+          value={full_name || ""}
+          onChange={(e) => setFullname(e.target.value)}
         />
       </div>
+      <div>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={username || ""}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <h4>Select Image</h4>
+      <input
+        type="file"
+        name="myImage"
+        onChange={(e) => storeProfilePic(e)}
+      />
 
       <div>
         <button
           className="button primary block"
-          onClick={() => updateProfile({ thisusername })}
+          onClick={() => {
+            updateProfile({ username });
+          }}
         >
           update
         </button>
