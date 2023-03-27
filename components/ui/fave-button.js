e52eea98ecad8cca1c126ea3edccfff19fca3686
Erik Tonnesen <estonnesen@gmail.com>
2023-03-27 10:13:07 -0400

    fixed nested links

--- components/ui/fave-button.js
+++ components/ui/fave-button.js
@@ -4,19 +4,18 @@ import FavoriteIcon from "../icons/favorite-icon";
 const FaveButton = ({ text, link }) => {
   return (
 
-    <div className={`w-3/4 flex flex-row p-2 border-solid border-2 hover:bg-slate-900 rounded-md m-1 hover:border-purple-600 hover:cursor-pointer border-slate-700`}>
-      <Link
+    <Link href={`/${link}`} className={`w-3/4 flex flex-row p-2 border-solid border-2 hover:bg-slate-900 rounded-md m-1 hover:border-purple-600 hover:cursor-pointer border-slate-700`}>
+      <div
         className="w-full text-base text-slate-300 hover:text-purple-400"
-        href={`/${link}`}
       >
         <h2 className="text-center">
           {text}
         </h2>
-      </Link>
+      </div>
       <button>
         <FavoriteIcon />
       </button>
-    </div>
+    </Link>
   );
 };
 
