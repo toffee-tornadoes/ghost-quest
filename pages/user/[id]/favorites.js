42ee3cb1ab9ab9a74df7f3aadd45ea7aad66fbd4
John Madrid <jmadr37@gmail.com>
2023-03-28 12:35:35 -0400

    Debug favoriting functionality

--- pages/user/[id]/favorites.js
+++ pages/user/[id]/favorites.js
@@ -1,37 +1,10 @@
-import { supabase } from "@/lib/supabaseClient";
 import LocationListingCard from "@/components/locations/loc-listing-card";
 import FavoritesHeader from "./favorites-header";
-import {
-  selectUserSavedLocs,
-  getUserSavedLocs,
-} from "@/slices/userSavedLocsSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { useState, useEffect } from "react";
+import { selectUserSavedLocs } from "@/slices/userSavedLocsSlice";
+import { useSelector } from "react-redux";
 
-// export const getServerSideProps = async (context) => {
-//   const { id } = context.params;
-//   const { data } = await supabase
-//     .from("user_locations")
-//     .select("*,locations(*)")
-//     .eq("profile_id", id)
-//     .eq("is_favorited", true);
-
-//   return {
-//     props: {
-//       data,
-//     },
-//   };
-// };
-
-// ghostquest.com/user/[id]/favorites
 const UserFavoritesPage = () => {
   const userSavedLocs = useSelector(selectUserSavedLocs);
-  // const [favs, setFavs] = useState([]);
-
-  // useEffect(() => {
-  //   const favLocs = findFavs();
-  //   setFavs(favLocs);
-  // }, [favs]);
 
   const findFavs = () => {
     const favLocs = [];
