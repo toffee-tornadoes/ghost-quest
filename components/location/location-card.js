835c5cf6324139d70c96128b1abad7ec53a8be03
rochbman <rochbman@gmail.com>
2023-03-29 14:32:35 -0400

    stars on screen

--- components/location/location-card.js
+++ components/location/location-card.js
@@ -2,12 +2,26 @@ import BackIcon from "../icons/back-icon";
 import { Fragment } from "react";
 import Link from "next/link";
 import { useSelector } from "react-redux";
+import { useState } from "react";
 import { selectUserComments } from "@/slices/userCommentsSlice";
+import StarRatings from "react-star-ratings";
+import { supabase } from "@/lib/supabaseClient";
 
 const LocationCard = ({ location  }) => {
   console.log(location)
   const userComments = useSelector(selectUserComments);
   console.log(userComments);
+  const [rating, setRating] = useState(0);
+
+    const ratingHandle = async () => {
+      try {
+        console.log(rating)
+        const { error } = await supabase.from("locations").insert([{  rating: [rating] }]).eq('id',`${location.id}`);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
   return (
     <div className="flex top-0 flex-col m-5 ">
       <div className="">
@@ -28,7 +42,17 @@ const LocationCard = ({ location  }) => {
       </div>
       <div className="bg-slate-800 m-5">
         <h1>Rating</h1>
-        <p>Something / 5 Stars</p>
+        <div className="flex flex-row">
+          <StarRatings
+            onChange={ratingHandle()}
+            rating={rating}
+            starRatedColor="purple"
+            starHoverColor='green'
+            changeRating={setRating}
+            numberOfStars={5}
+            name="rating"
+          />
+        </div>
       </div>
       <div className="bg-slate-800 m-5">
         <h1>Fear Factor</h1>
@@ -41,24 +65,25 @@ const LocationCard = ({ location  }) => {
       <div>
         <div className="overflow-auto content-center max-h-screen ">
           {userComments?.map((comment) => {
-            if(comment.location_id==location.id){
-            return (
-              <Fragment key={comment.id}>
-                <div className="p-6 m-6 max-w-sm mx-auto bg-purple-500 rounded-xl shadow-lg flex-col  items-center space-x-4 shadow-green-400">
-                  {comment.profiles && (
-                    <Link
-                      className="p-6 text-lg hover:text-slate-300 "
-                      href={`/user/${comment.profiles.id}`}
-                    >
-                      {comment.profiles.username}
-                    </Link>
-                  )}
-                  <div className="border-solid border-2 rounded-lg mt-4 border-indigo-600">
-                    {comment.content}
+            if (comment.location_id == location.id) {
+              return (
+                <Fragment key={comment.id}>
+                  <div className="p-6 m-6 max-w-sm mx-auto bg-purple-500 rounded-xl shadow-lg flex-col  items-center space-x-4 shadow-green-400">
+                    {comment.profiles && (
+                      <Link
+                        className="p-6 text-lg hover:text-slate-300 "
+                        href={`/user/${comment.profiles.id}`}
+                      >
+                        {comment.profiles.username}
+                      </Link>
+                    )}
+                    <div className="border-solid border-2 rounded-lg mt-4 border-indigo-600">
+                      {comment.content}
+                    </div>
                   </div>
-                </div>
-              </Fragment>
-            );}
+                </Fragment>
+              );
+            }
           })}
         </div>
       </div>
