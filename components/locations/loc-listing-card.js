f281b886126cede85f00018947d95afb2df41622
John Madrid <jmadr37@gmail.com>
2023-04-01 09:41:52 -0400

    Debug toast notification for favoriting

--- components/locations/loc-listing-card.js
+++ components/locations/loc-listing-card.js
@@ -79,24 +79,24 @@ const LocationListingCard = ({ locations }) => {
                         favStatus={favStatus}
                       />
                     </button>
-                    <ToastContainer
-                      position="top-center"
-                      autoClose={5000}
-                      hideProgressBar={false}
-                      newestOnTop={false}
-                      closeOnClick
-                      rtl={false}
-                      pauseOnFocusLoss
-                      draggable
-                      pauseOnHover
-                      theme="dark"
-                    />
                   </>
                 ) : null}
               </div>
             </Fragment>
           );
         })}
+        <ToastContainer
+          position="top-center"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
       </div>
     </div>
   );
