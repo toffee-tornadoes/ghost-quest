1489905654add11f818fa983f5654333b2ed0dad
Erik Tonnesen <estonnesen@gmail.com>
2023-04-01 08:39:38 -0400

    updated user profile image style

--- components/user/user-header.js
+++ components/user/user-header.js
@@ -9,17 +9,26 @@ const UserHeader = ({ profile, pic }) => {
   const dispatch = useDispatch();
   const userProfile = useSelector(selectUserProfile);
 
-  useEffect(()=>{
-    dispatch(fetchUserProfile(user?.id))
-  }, [])
-  console.log(userProfile?.profile_pic)
+  useEffect(() => {
+    dispatch(fetchUserProfile(user?.id));
+  }, []);
+  console.log(userProfile?.profile_pic);
   return (
     <div className="flex justify-between border-b mb-5" id="locListingHeader">
       <div className="m-2 text-left text-3xl">
-        <img src={userProfile?.profile_pic} alt="Profile pic" />
-        <h1>{userProfile?.username}</h1>
-        <p className="text-slate-500 italic text-base">{user?.email}</p>
-
+        <div className="flex flex-row gap-2 items-end">
+          <div className="m-2 rounded-full w-16 h-16 border-2 border-slate-300 overflow-clip">
+            <img
+              className=""
+              src={userProfile?.profile_pic}
+              alt="Profile pic"
+            />
+          </div>
+          <div className="flex flex-col">
+            <h1>{userProfile?.username}</h1>
+            <p className="text-slate-500 italic text-base">{user?.email}</p>
+          </div>
+        </div>
       </div>
       <div className="p-2">
         <BackIcon />
